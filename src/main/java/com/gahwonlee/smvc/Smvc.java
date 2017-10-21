36059d79bb3e117bf76bfa5f72707ba0626687e6
creativitRy <creativitRy@gmail.com>
2017-10-20 22:56:49 -0500

    start on gui

--- src/main/java/com/gahwonlee/smvc/Smvc.java
+++ src/main/java/com/gahwonlee/smvc/Smvc.java
@@ -20,6 +20,9 @@ package com.gahwonlee.smvc;
 
 import io.humble.video.Rational;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
@@ -29,6 +32,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.TreeMap;
 
 /**
@@ -38,6 +42,7 @@ import java.util.TreeMap;
  */
 public class Smvc extends Application {
 	private static Logger logger = LogManager.getLogger();
+	private AnchorPane mainGui;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -45,41 +50,46 @@ public class Smvc extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-		System.out.println("hi");
-		System.out.println(System.getProperty("java.class.path"));
-		logger.error("hello world");
-		logger.debug("hello world");
-		test();
-		System.out.println(System.getProperty("log4j.configurationFile"));
-		
-		primaryStage.show();
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/Main.fxml"));
+			mainGui = loader.load();
+			
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(mainGui);
+			primaryStage.setScene(scene);
+			primaryStage.setMaximized(true);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setInitialDirectory(new File("C:\\Users\\creativitRy\\Documents\\minecraft\\maps\\_mymaps\\water\\processed"));
-		File file = fileChooser.showOpenDialog(null);
-		File file2 = fileChooser.showOpenDialog(null);
-		File file3 = fileChooser.showOpenDialog(null);
 		
 		TreeMap<Rational, BufferedImage> map = new TreeMap<>(new RationalComparator());
 		try {
+			File file = fileChooser.showOpenDialog(primaryStage);
+			if (file == null)
+				return;
 			map.put(Rational.make(0, 1), ImageIO.read(file));
+			File file2 = fileChooser.showOpenDialog(primaryStage);
+			if (file2 == null)
+				return;
 			map.put(Rational.make(1, 1), ImageIO.read(file2));
+			File file3 = fileChooser.showOpenDialog(primaryStage);
+			if (file3 == null)
+				return;
 			map.put(Rational.make(3, 1), ImageIO.read(file3));
 			
 			VideoEncoder videoEncoder = new VideoEncoder(
-					fileChooser.showSaveDialog(null),
+					fileChooser.showSaveDialog(primaryStage),
 					map,
 					new VideoProperties(Rational.make(1, 24), 960, 540, Rational.make(6, 1)));
 			videoEncoder.run();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
+		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	
-	private static void test() {
-		logger.error("lol");
 	}
 }
